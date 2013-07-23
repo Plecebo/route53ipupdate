86782b2b3f5237a5077ae898763bba70318a088f
Larry Schwerzler <larry@schwerzler.com>
2013-07-23 20:26:02 +0000

    Adding subdomain functionality

--- ip.py
+++ ip.py
@@ -9,14 +9,25 @@ from urllib import urlopen
 from slick53 import route53
 import re
 
+#host zone to update in AWS Route 53
+host = 'wavepointmedia.com';
+#subdomain record set (A record) use empty string to update root domain
+
+#domain = 'example.wavepointmedia.com';
+domain = '';
+
+#No changes needed beyond this point
+
 #Fetch public IP of this server
 ip = urlopen('http://www.wavepointmedia.com/ip/').read();
 
-#domain zone to update in AWS Route 53
-domain = 'wavepointmedia.net';
 print 'Public IP:', ip
 
-zone = route53.get_zone(domain)
+# if the domain is an empty string use the host
+if not domain: 
+	domain = host
+
+zone = route53.get_zone(host)
 aRecord =  str(zone.get_a(domain))
 awsIP = re.findall( r'[0-9]+(?:\.[0-9]+){3}', aRecord )
 print 'AWS IP:', awsIP[0]
